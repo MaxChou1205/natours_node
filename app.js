bfa78a97c0e236ef10a023b0dfdc84e926bd84ad
MaxChou1205 <v2asault@yahoo.com.tw>
2022-12-19 22:18:23 +0800

    add post request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -11,6 +12,23 @@ app.get('/api/v1/tours', (req, res) => {
     status: 'ok',
     data: tours,
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      res.status(200).json({
+        status: 'ok',
+        data: newTour,
+      });
+    }
+  );
 });
 
 app.listen(3000, () => {
