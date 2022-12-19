bf4cb1b0f1d953d7c8b32424bd68d246e45a97e8
MaxChou1205 <v2asault@yahoo.com.tw>
2022-12-19 21:46:17 +0800

    add get feature

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    data: tours,
+  });
 });
 
 app.listen(3000, () => {
