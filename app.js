c6f3cf22be680fd9b281d8fad67035d605719c0d
MaxChou1205 <v2asault@yahoo.com.tw>
2022-12-20 22:03:04 +0800

    response to url parameter

--- app.js
+++ app.js
@@ -7,10 +7,26 @@ app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'ok',
     data: tours,
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((item) => item.id === id);
+  if (!tour)
+    return res.status(404).json({
+      status: 'not found',
+      message: 'Invalid ID',
+    });
+
+  res.status(200).json({
+    status: 'ok',
+    data: tour,
   });
 });
 
