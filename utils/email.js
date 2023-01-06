08b1a6e29b42e2236a63aec4427c969823a80f68
MaxChou1205 <v2asault@yahoo.com.tw>
2023-01-06 22:55:27 +0800

    delete current user

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async options => {
 
   // 2) Define the email options
   const mailOptions = {
-    from: 'Jonas Schmedtmann <hello@jonas.io>',
+    from: 'Max Chou',
     to: options.email,
     subject: options.subject,
     text: options.message
