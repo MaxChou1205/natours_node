62ece62a9b81947c0593b6ca64087d2f2698711c
MaxChou1205 <v2asault@yahoo.com.tw>
2023-01-17 20:26:06 +0800

    tour detail page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,13 +3,7 @@ const viewController = require('../controllers/viewController');
 
 const router = express.Router();
 
-router.get('/overview', viewController.getOverview);
-router.get('/tour', viewController.getTour);
-router.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas',
-  });
-});
+router.get('/', viewController.getOverview);
+router.get('/tour/:slug', viewController.getTour);
 
 module.exports = router;
