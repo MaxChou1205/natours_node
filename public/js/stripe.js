acb8e554597b8ac719a033e394fcc5f62b0b5e06
MaxChou1205 <v2asault@yahoo.com.tw>
2023-02-03 22:11:00 +0800

    for deploy

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,16 +7,15 @@ export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
     const session = await axios(
-      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
+      `/api/v1/bookings/checkout-session/${tourId}`
     );
-    console.log(session);
 
     // 2) Create checkout form + chanre credit card
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id
     });
   } catch (err) {
-    console.log(err);
+    console.error(err);
     showAlert('error', err);
   }
 };
